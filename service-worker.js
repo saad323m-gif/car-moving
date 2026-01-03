0527daa7e461d862c91e45acf815f67ea1d9e8d5
saad323m-gif <saad323m@gmail.com>
2026-01-04 02:19:36 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,14 @@
-const CACHE_NAME = "car-mgmt-cache-v1";
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./logo.png"
-];
+const CACHE_NAME = 'masaood-v1';
+const urlsToCache = ['index.html', 'print-styles.css', 'logo.png'];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
